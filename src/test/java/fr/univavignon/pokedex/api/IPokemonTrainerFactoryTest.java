499e1f1be3fed5bb05a81f169e0986696ce43116
NicolasUrban2 <nclsurban@gmail.com>
2024-03-05 11:19:34 +0100

    Remove exception thrown

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -12,7 +12,7 @@ public class IPokemonTrainerFactoryTest {
     IPokemonTrainerFactory pokemonTrainerFactory = mock(IPokemonTrainerFactory.class);
 
     @BeforeEach
-    public void setUp() throws Exception {
+    public void setUp() {
         pokemonTrainerFactory = mock(IPokemonTrainerFactory.class);
     }
 
